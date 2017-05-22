9e80a8e24f3c6bf85f1b3fe4ecb6ce8dcd87fe81
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-22 21:55:14 +0800

    Implant photo filter example

---
+++ app/src/main/java/io/imagineer/instagramclone/ThumbnailCallback.java
@@ -0,0 +1,8 @@
+package io.imagineer.instagramclone;
+
+import com.zomato.photofilters.imageprocessors.Filter;
+
+public interface ThumbnailCallback {
+
+    void onThumbnailClick(Filter filter);
+}
---
+++ app/src/main/java/io/imagineer/instagramclone/ThumbnailItem.java
@@ -0,0 +1,15 @@
+package io.imagineer.instagramclone;
+
+import android.graphics.Bitmap;
+
+import com.zomato.photofilters.imageprocessors.Filter;
+
+public class ThumbnailItem {
+    public Bitmap image;
+    public Filter filter;
+
+    public ThumbnailItem() {
+        image = null;
+        filter = new Filter();
+    }
+}
