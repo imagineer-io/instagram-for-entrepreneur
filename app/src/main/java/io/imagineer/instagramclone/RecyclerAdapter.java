4c4fb3eec784e465dcca34069fde8e39ce12b110
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-25 00:18:46 +0800

    Add recycler sample activity, adapter

---
+++ app/src/main/java/io/imagineer/instagramclone/RecyclerAdapter.java
@@ -0,0 +1,10 @@
+package io.imagineer.instagramclone;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by marco on 5/25/17.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter {
+}
