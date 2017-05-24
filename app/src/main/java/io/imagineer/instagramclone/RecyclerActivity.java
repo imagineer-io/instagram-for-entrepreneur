a59c7d49493ea3ea4693f8e722cff4a5824082f2
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-25 00:50:08 +0800

    Implement RecyclerActivity

--- app/src/main/java/io/imagineer/instagramclone/RecyclerActivity.java
+++ app/src/main/java/io/imagineer/instagramclone/RecyclerActivity.java
@@ -1,9 +1,14 @@
 package io.imagineer.instagramclone;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class RecyclerActivity extends AppCompatActivity {
 
@@ -17,5 +22,41 @@ public class RecyclerActivity extends AppCompatActivity {
 
         mImageView = (ImageView) findViewById(R.id.recyclerImageView);
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // 1) mImageView 에 이미지 집어넣기
+        mImageView.setImageBitmap(Bitmap.createScaledBitmap(
+                BitmapFactory.decodeResource(
+                        this.getApplicationContext().getResources(),
+                        R.drawable.photo),
+                640, 640, false));
+
+        // 2-1) mRecyclerView 에 레이아웃 세팅하기
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        layoutManager.scrollToPosition(0);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        // 2-2) mRecyclerView 에 어뎁터 세팅하기
+        Bitmap thumbImage = Bitmap.createScaledBitmap(
+                BitmapFactory.decodeResource(getResources(), R.drawable.photo),
+                640, 640, false);
+
+        ArrayList<ThumbnailItem> list = new ArrayList<>();
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+        list.add(new ThumbnailItem());
+
+        for(ThumbnailItem item : list) {
+            item.image = thumbImage;
+        }
+
+        RecyclerAdapter adapter = new RecyclerAdapter(list);
+        mRecyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
