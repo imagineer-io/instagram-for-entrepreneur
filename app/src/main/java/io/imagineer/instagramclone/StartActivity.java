edf9125eeb3da432ea95b57650cbe0d7a04a6aa7
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-24 14:19:30 +0800

    Add an intent to gallery

---
+++ app/src/main/java/io/imagineer/instagramclone/StartActivity.java
@@ -0,0 +1,31 @@
+package io.imagineer.instagramclone;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        mButton = (Button) findViewById(R.id.buttonToGallery);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+    }
+}
