33a2d68738ec30edcadcffae53671138689c3d15
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-25 00:35:27 +0800

    Implement RecyclerAdapter

--- app/src/main/java/io/imagineer/instagramclone/RecyclerActivity.java
+++ app/src/main/java/io/imagineer/instagramclone/RecyclerActivity.java
@@ -2,12 +2,20 @@ package io.imagineer.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        mImageView = (ImageView) findViewById(R.id.recyclerImageView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
 }
--- app/src/main/java/io/imagineer/instagramclone/RecyclerAdapter.java
+++ app/src/main/java/io/imagineer/instagramclone/RecyclerAdapter.java
@@ -1,10 +1,50 @@
 package io.imagineer.instagramclone;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
 
 /**
  * Created by marco on 5/25/17.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<ThumbnailItem> mThumbnailItems;
+
+    public RecyclerAdapter(List<ThumbnailItem> thumbnailItems) {
+        mThumbnailItems = thumbnailItems;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.list_thumbnail_item, parent, false);
+        return new RecyclerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final ThumbnailItem thumbnailItem = mThumbnailItems.get(position);
+        RecyclerViewHolder viewHolder = (RecyclerViewHolder) holder;
+        viewHolder.thumbnail.setImageBitmap(thumbnailItem.image);
+        viewHolder.thumbnail.setScaleType(ImageView.ScaleType.FIT_START);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mThumbnailItems.size();
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        private ImageView thumbnail;
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+        }
+    }
 }
